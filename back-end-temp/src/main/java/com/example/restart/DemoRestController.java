fbb1ed44d9e51e1cc36eff874063238fb5b8c8dd
Krdmn9 <58071725+Krdmn9@users.noreply.github.com>
2023-05-21 00:59:04 +0300

    election sayfasına backend baglantisi

--- back-end-temp/src/main/java/com/example/restart/DemoRestController.java
+++ back-end-temp/src/main/java/com/example/restart/DemoRestController.java
@@ -24,11 +24,11 @@ public class DemoRestController {
 
     @GetMapping("/students")
     private List<Student> loadData() {
-        List<Student> list= new ArrayList<>();
+        List<Student> list= new ArrayList<>();  
         // create employees
         Student emp1 = new Student(1,"Emre","Karaduman", 3.5,"Computer Engineering","description1",25);
         Student emp2 = new Student(2,"Halil","Uyanik", 2.6,"Civil Engineering","description2",15);
-        Student emp3 = new Student(3,"Gencay","Turgut", 3.2,"Computer Engineering","description3",20);
+        Student emp3 = new Student(3,"Gencay","Turgut", 3.2,"Electronic Engineering","description3",20);
         Student emp4 = new Student(4,"Ahmet","Özdemir", 2.8,"Computer Engineering","description4",10);
 
         // create the list
--- ui/src/Pages/Election.js
+++ ui/src/Pages/Election.js
@@ -4,56 +4,31 @@ import { Chart } from "react-google-charts";
 import axios from "axios";
 
 function Election() {
-  const [department, setDepartment] = useState("computer");
+  const [department, setDepartment] = useState()
   const [candidates, setCandidates] = useState([]);
-
-  const [data, setData] = useState([]);
+  const [filteredCandidates, setFilteredCandidates] = useState([]);
   const url = "http://localhost:8080/students";
+  console.log(candidates)
   const fetchInfo = async () => {
     const response = await axios.get(url);
-
-    return setData(response.data);
+    setCandidates(response.data);
   };
-  console.log(data);
   useEffect(() => {
     fetchInfo();
-    let candidates = [];
-    candidates = [
-      ...data.map((candidate) => [candidate.name, candidate.percentage]),
-    ];
-    // console.log(candidates)
   }, []);
 
-  // useEffect(() => {
-  //   let transformedCandidates = [];
-  //   if (department === "computer") {
-  //     transformedCandidates = [
-  //       ["Name", "Percentage"],
-  //       ...computerEngineeringCandidates.map((candidate) => [
-  //         candidate.name,
-  //         candidate.percentage,
-  //       ]),
-  //     ];
-  //   } else if (department === "civil") {
-  //     transformedCandidates = [
-  //       ["Name", "Percentage"],
-  //       ...civilEngineeringCandidates.map((candidate) => [
-  //         candidate.name,
-  //         candidate.percentage,
-  //       ]),
-  //     ];
-  //   }
-  //   setCandidates(transformedCandidates);
-  // }, [department]);
 
   useEffect(() => {
     const transformedCandidates = [
       ["Name", "Percentage"],
-      ...data.map((candidate) => [candidate.name, candidate.percentage]),
+      ...candidates
+        .filter(candidate => candidate.department === department)
+        .map(candidate => [candidate.firstName, candidate.currentVote]),
     ];
-
-    setCandidates(transformedCandidates);
+  
+    setFilteredCandidates(transformedCandidates);
   }, [department]);
+  
 
   const chartHandler = (event) => {
     setDepartment(event.target.value);
@@ -63,17 +38,20 @@ function Election() {
     title: "Election Results",
     is3D: true,
   };
-
+  console.log(filteredCandidates.length)
   return (
     <div className="container">
       <form>
         <label htmlFor="department">Department:</label>
         <select name="department" id="department" onChange={chartHandler}>
-          <option value="computer">Computer Engineering</option>
-          <option value="civil">Civil Engineering</option>
+          <option>Please select a department</option>
+          <option value="Computer Engineering">Computer Engineering</option>
+          <option value="Civil Engineering">Civil Engineering</option>
+          <option value="Electronic Engineering">Electronic Engineering</option>
         </select>
       </form>
-      <Chart chartType="PieChart" data={candidates} options={options} />
+      {filteredCandidates.length > 1 && <Chart chartType="PieChart" data={filteredCandidates} options={options} />}
+
     </div>
   );
 }
