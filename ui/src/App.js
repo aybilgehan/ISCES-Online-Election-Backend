22fb49804a9c01e47154cbc31f3b6fd3d110aa58
kn1ghtm4r3jj <halil.can023@gmail.com>
2023-05-30 04:20:52 +0300

    background and container structures have been changed

--- ui/src/App.js
+++ ui/src/App.js
@@ -19,34 +19,34 @@ function App() {
 
   return (
     <div className="app-container">
-      <img
-        src="https://bhib.iyte.edu.tr/wp-content/uploads/sites/115/2018/09/iyte_logo-tur.png"
-        style={{ width: '200px', height: 'auto', margin: '0 auto', display: 'block' }}
-        alt="IYTE Logo"
-      />
-
       <BrowserRouter>
-        <div className="content-container">
-          {authCtx.isLoggedIn && <SideBar />}
-          <Routes>
-            {!authCtx.isLoggedIn && <Route path="/*" element={<Login />} />}
-            {authCtx.isLoggedIn && (
-              <>
+        {authCtx.isLoggedIn && <SideBar />}
+        <Routes>
+          {!authCtx.isLoggedIn && <Route path="/*" element={<Login />} />}
+          {authCtx.isLoggedIn && (
+            <>
               {/* Buraya auth context ile kullanıcıların hangi sayfaları görebileceğini ekleyeceğiz. 
               Şu an kullanıcı giriş yapmamışken hiçbir sayfaya giremiyor karşısına hep login çıkacak.
               Giriş yaptıktan sonra öğrenci ve görevlilere farklı butonlar aktif olacak. Inline if state'i ve 
               authcontext rol kontrolü ile bunu sağlayacağız. */}
-                {authCtx.userRole=="student"&&<Route path="/council" element={<Council />} />}
-                {authCtx.userRole=="rector"&&<Route path="/setelectiondate" element={< SetElectionDate/>} />}
-                <Route path="/home" element={<Home time={blabla} />} />
-                {authCtx.userRole == "student" && <Route path="/candidates" element={<Candidates />} />}
-                <Route path="/election" element={<Election />} />
-                {authCtx.userRole=="student"&&<Route path="/candidateform" element={<CandidateForm />} />}
-                <Route path="/profile" element={<Profile />} />
-              </>
-            )}
-          </Routes>
-        </div>
+              {authCtx.userRole == "student" && (
+                <Route path="/council" element={<Council />} />
+              )}
+              {authCtx.userRole == "rector" && (
+                <Route path="/setelectiondate" element={<SetElectionDate />} />
+              )}
+              <Route path="/home" element={<Home time={blabla} />} />
+              {authCtx.userRole == "student" && (
+                <Route path="/candidates" element={<Candidates />} />
+              )}
+              <Route path="/election" element={<Election />} />
+              {authCtx.userRole == "student" && (
+                <Route path="/candidateform" element={<CandidateForm />} />
+              )}
+              <Route path="/profile" element={<Profile />} />
+            </>
+          )}
+        </Routes>
       </BrowserRouter>
     </div>
   );
