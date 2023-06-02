07f4c9fea661bab709c96f1f6edc57af8fdf93a5
ahmetozdemir99 <ahmetozdemir99@hotmail.com>
2023-06-02 20:01:20 +0300

    updated sql script

--- ISCES/ISCES_TABLE_SCRIPT.sql
+++ ISCES/ISCES_TABLE_SCRIPT.sql
@@ -49,19 +49,50 @@ CREATE TABLE `candidate` (
 
 INSERT INTO `user` 
 VALUES 
-('ahmetozdemir@std.iyte.edu.tr','test123','student'),
-('bilgehanay@std.iyte.edu.tr','test123','candidate'),
-('ecesavran@std.iyte.edu.tr','test123','student'),
-('emrekaraduman@std.iyte.edu.tr','test123','candidate');
+('ahmetozdemir1@std.iyte.edu.tr','test123','student'),
+('ahmetozdemir2@std.iyte.edu.tr','test123','student'),
+('ahmetozdemir3@std.iyte.edu.tr','test123','student'),
+('ahmetozdemir4@std.iyte.edu.tr','test123','student'),
+('ahmetozdemir5@std.iyte.edu.tr','test123','student'),
+('bilgehanay1@std.iyte.edu.tr','test123','student'),
+('bilgehanay2@std.iyte.edu.tr','test123','student'),
+('bilgehanay3@std.iyte.edu.tr','test123','student'),
+('bilgehanay4@std.iyte.edu.tr','test123','student'),
+('bilgehanay5@std.iyte.edu.tr','test123','student'),
+('ecesavran1@std.iyte.edu.tr','test123','student'),
+('ecesavran2@std.iyte.edu.tr','test123','student'),
+('ecesavran3@std.iyte.edu.tr','test123','student'),
+('ecesavran4@std.iyte.edu.tr','test123','student'),
+('ecesavran5@std.iyte.edu.tr','test123','student'),
+('emrekaraduman1@std.iyte.edu.tr','test123','student'),
+('emrekaraduman2@std.iyte.edu.tr','test123','student'),
+('emrekaraduman3@std.iyte.edu.tr','test123','student'),
+('emrekaraduman4@std.iyte.edu.tr','test123','student'),
+('emrekaraduman5@std.iyte.edu.tr','test123','student');
 
 
 INSERT INTO `student` 
 VALUES 
-(270201050,1,'Ahmet','Özdemir',2.73,3,0,'ahmetozdemir@std.iyte.edu.tr',false),
-(270201020,1,'Ece','Savran',3.41,3,0,'ecesavran@std.iyte.edu.tr',false),
-(270201030,1,'Emre','Karaduman',3.41,3,0,'emrekaraduman@std.iyte.edu.tr',false),
-(270201070,1,'Bilgehan','Ay',3.41,3,0,'bilgehanay@std.iyte.edu.tr',false);
-
+(270201051, 1, 'Ahmet', 'Özdemir', 2.73, 3, 0, 'ahmetozdemir1@std.iyte.edu.tr', false),
+(270201052, 1, 'Ahmet', 'Özdemir', 2.73, 3, 0, 'ahmetozdemir2@std.iyte.edu.tr', false),
+(270201053, 1, 'Ahmet', 'Özdemir', 2.73, 3, 0, 'ahmetozdemir3@std.iyte.edu.tr', false),
+(270201054, 1, 'Ahmet', 'Özdemir', 2.73, 3, 0, 'ahmetozdemir4@std.iyte.edu.tr', false),
+(270201055, 1, 'Ahmet', 'Özdemir', 2.73, 3, 0, 'ahmetozdemir5@std.iyte.edu.tr', false),
+(270201021, 2, 'Bilgehan', 'Ay', 3.41, 3, 0, 'bilgehanay1@std.iyte.edu.tr', false),
+(270201022, 2, 'Bilgehan', 'Ay', 3.41, 3, 0, 'bilgehanay2@std.iyte.edu.tr', false),
+(270201023, 2, 'Bilgehan', 'Ay', 3.41, 3, 0, 'bilgehanay3@std.iyte.edu.tr', false),
+(270201024, 2, 'Bilgehan', 'Ay', 3.41, 3, 0, 'bilgehanay4@std.iyte.edu.tr', false),
+(270201025, 2, 'Bilgehan', 'Ay', 3.41, 3, 0, 'bilgehanay5@std.iyte.edu.tr', false),
+(270201031, 3, 'Ece', 'Savran', 3.41, 3, 0, 'ecesavran1@std.iyte.edu.tr', false),
+(270201032, 3, 'Ece', 'Savran', 3.41, 3, 0, 'ecesavran2@std.iyte.edu.tr', false),
+(270201033, 3, 'Ece', 'Savran', 3.41, 3, 0, 'ecesavran3@std.iyte.edu.tr', false),
+(270201034, 3, 'Ece', 'Savran', 3.41, 3, 0, 'ecesavran4@std.iyte.edu.tr', false),
+(270201035, 3, 'Ece', 'Savran', 3.41, 3, 0, 'ecesavran5@std.iyte.edu.tr', false),
+(270201071, 4, 'Emre', 'Karaduman', 3.41, 3, 0, 'emrekaraduman1@std.iyte.edu.tr', false),
+(270201072, 4, 'Emre', 'Karaduman', 3.41, 3, 0, 'emrekaraduman2@std.iyte.edu.tr', false),
+(270201073, 4, 'Emre', 'Karaduman', 3.41, 3, 0, 'emrekaraduman3@std.iyte.edu.tr', false),
+(270201074, 4, 'Emre', 'Karaduman', 3.41, 3, 0, 'emrekaraduman4@std.iyte.edu.tr', false),
+(270201075, 4, 'Emre', 'Karaduman', 3.41, 3, 0, 'emrekaraduman5@std.iyte.edu.tr', false);
 
 
 
