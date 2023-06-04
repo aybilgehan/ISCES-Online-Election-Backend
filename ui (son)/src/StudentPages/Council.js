c528ec38e9a0b114687398fe6b3f6060c4cddc28
Ece Savran <ecesvrn00@gmail.com>
2023-06-04 21:39:31 +0300

    council css, font değişti

--- ui (son)/src/StudentPages/Council.js
+++ ui (son)/src/StudentPages/Council.js
@@ -1,13 +1,153 @@
-import "./Council.css"
+import "./Council.css";
 export default function Council() {
-    return (
-        <div className="container">
-          <div className="department-representative">
-            Current Department Representative: "Steve Jobs"
-          </div>
-          <div className="faculty-representative">
-            Current Faculty Representative: "Graham Bell"
+  return (
+    <div className="council-container">
+      <h1 className="our-council">OUR COUNCIL</h1>
+      <h2 className="member-title">Counsil President</h2>
+      <div className="council-member council-president">
+        <div className="council-member-photo"></div>
+        <div className="council-member-info">
+          <div className="council-member-name">Ahmet Yılmaz</div>
+          <div className="council-member-department">Computer Engineering</div>
+          <div className="council-member-contact">
+            mehmetyilmaz@std.iyte.edu.tr
           </div>
         </div>
-      );
+      </div>
+      <h2 className="member-title">Faculty Representatives</h2>
+      <div className="council-member faculty-representative">
+        <div className="council-member-photo"></div>
+        <div className="council-member-info">
+          <div className="council-member-name">Zeynep Demir</div>
+          <div className="council-member-department">Industrial Design</div>
+          <div className="council-member-contact">
+            zeynepdemir@std.iyte.edu.tr
+          </div>
+        </div>
+      </div>
+      <div className="council-member faculty-representative">
+        <div className="council-member-photo"></div>
+        <div className="council-member-info">
+          <div className="council-member-name">Bilge Akın</div>
+          <div className="council-member-department">
+            Mechanical Engineering
+          </div>
+          <div className="council-member-contact">
+            bilgeakin@std.iyte.edu.tr
+          </div>
+        </div>
+      </div>
+      <div className="council-member faculty-representative">
+        <div className="council-member-photo"></div>
+        <div className="council-member-info">
+          <div className="council-member-name">Umut Giray</div>
+          <div className="council-member-department">Physics</div>
+          <div className="council-member-contact">
+            hakanartuk@std.iyte.edu.tr
+          </div>
+        </div>
+      </div>
+      <h2 className="member-title">Department Representatives</h2>
+      <div className="council-member department-representative">
+        <div className="council-member-photo"></div>
+        <div className="council-member-info">
+          <div className="council-member-name">Fatih Arik</div>
+          <div className="council-member-department">Physics</div>
+          <div className="council-member-contact">
+            hakanartuk@std.iyte.edu.tr
+          </div>
+        </div>
+      </div>
+      <div className="council-member department-representative">
+        <div className="council-member-photo"></div>
+        <div className="council-member-info">
+          <div className="council-member-name">Ali Göker</div>
+          <div className="council-member-department">Computer Engineering</div>
+          <div className="council-member-contact">
+            hakanartuk@std.iyte.edu.tr
+          </div>
+        </div>
+      </div>
+      <div className="council-member department-representative">
+        <div className="council-member-photo"></div>
+        <div className="council-member-info">
+          <div className="council-member-name">Ali Göker</div>
+          <div className="council-member-department">Computer Engineering</div>
+          <div className="council-member-contact">
+            hakanartuk@std.iyte.edu.tr
+          </div>
+        </div>
+      </div>
+      <div className="council-member department-representative">
+        <div className="council-member-photo"></div>
+        <div className="council-member-info">
+          <div className="council-member-name">Ali Göker</div>
+          <div className="council-member-department">Computer Engineering</div>
+          <div className="council-member-contact">
+            hakanartuk@std.iyte.edu.tr
+          </div>
+        </div>
+      </div>
+      <div className="council-member department-representative">
+        <div className="council-member-photo"></div>
+        <div className="council-member-info">
+          <div className="council-member-name">Ali Göker</div>
+          <div className="council-member-department">Computer Engineering</div>
+          <div className="council-member-contact">
+            hakanartuk@std.iyte.edu.tr
+          </div>
+        </div>
+      </div>
+      <div className="council-member department-representative">
+        <div className="council-member-photo"></div>
+        <div className="council-member-info">
+          <div className="council-member-name">Ali Göker</div>
+          <div className="council-member-department">Computer Engineering</div>
+          <div className="council-member-contact">
+            hakanartuk@std.iyte.edu.tr
+          </div>
+        </div>
+      </div>
+      <div className="council-member department-representative">
+        <div className="council-member-photo"></div>
+        <div className="council-member-info">
+          <div className="council-member-name">Ali Göker</div>
+          <div className="council-member-department">Computer Engineering</div>
+          <div className="council-member-contact">
+            hakanartuk@std.iyte.edu.tr
+          </div>
+        </div>
+      </div>
+      <div className="council-member department-representative">
+        <div className="council-member-photo"></div>
+        <div className="council-member-info">
+          <div className="council-member-name">Ali Göker</div>
+          <div className="council-member-department">Computer Engineering</div>
+          <div className="council-member-contact">
+            hakanartuk@std.iyte.edu.tr
+          </div>
+        </div>
+      </div>
+      <div className="council-member department-representative">
+        <div className="council-member-photo"></div>
+        <div className="council-member-info">
+          <div className="council-member-name">Ali Göker</div>
+          <div className="council-member-department">Computer Engineering</div>
+          <div className="council-member-contact">
+            hakanartuk@std.iyte.edu.tr
+          </div>
+        </div>
+      </div>
+      <div className="council-member department-representative">
+        <div className="council-member-photo"></div>
+        <div className="council-member-info">
+          <div className="council-member-name">Ali Göker</div>
+          <div className="council-member-department">Computer Engineering</div>
+          <div className="council-member-contact">
+            hakanartuk@std.iyte.edu.tr
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
