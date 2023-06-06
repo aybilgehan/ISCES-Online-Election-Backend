0def6eaa494478af73620c98f94d311d36dac1c3
Gencay Turgut <gencayturgut2@gmail.com>
2023-06-07 00:51:28 +0300

    123karaduman123

--- ui/src/Pages/Login.js
+++ ui/src/Pages/Login.js
@@ -2,7 +2,6 @@ import React, { useState, useContext } from "react";
 import AuthContext from "../context/AuthContext";
 import "./Login.css";
 import axios from "axios";
-import fetch from "node-fetch";
 const Login = () => {
   const alertBox = (
     <div className="AlertBox">
@@ -20,18 +19,11 @@ const Login = () => {
   }
   const submitHandler = async (event) => {
     event.preventDefault();
-    const email = enteredEmail.trim();
-    const password = enteredPassword.trim();
-    try {
-      fetch('https://iztechelection.herokuapp.com/login', {  // Enter your IP address here
-
-      method: 'POST', 
-      mode: 'cors', 
-      res: JSON.stringify({email:email,password:password}) // body data type must match "Content-Type" header
-
-    })
-     
-
+  const email = enteredEmail.trim();
+  const password = enteredPassword.trim();
+  try {
+    const activationURL = "https://iztechelection.herokuapp.com/login";
+    const res = await axios.post(activationURL, { email, password });
       if (res.status === 200) {
         let returned = res.data;
         if (returned.role === "candidate") {
